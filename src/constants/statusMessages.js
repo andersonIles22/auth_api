4111136e58eeebe73cd71e9015d431abfe85c94b
Anderson <ilesanderson01@gmail.com>
2026-01-13 17:01:55 -0500

    Implementación de registros de usuario con JWT

--- src/constants/statusMessages.js
+++ src/constants/statusMessages.js
@@ -1,19 +1,17 @@
 
 const MESSAGES_OPERATION= Object.freeze({
     SUCCESFUL_OPERATION:"Successful Operation",
-    NOTE_CREATED:"Note created successfully",
-    NOTE_UPDATED:"Note updated successfully",
-    NOTE_DELETED:"Note deleted successfullt",
     TITTLE_REQUIRED:"Title is required",
     SERVER_ERROR:"Internal Server Error",
+    EMAIL_ALREADY_EXIST:"Email already registered",
     URL_NO_FOUND:(url)=>`Path: ${url}, no Found`
 });
 const MESSAGES_VALIDATION=({
-    TTTLE_TOO_LONG:"Title must be 200 chareacters or less",
-    CONTENT_REQUIRED:"Content is required",
-    CONTENT_TOO_LONG:"Content must be 200 chareacters or less",
-    ID_POSITIVE:"ID Value  just must be an Positive Integer",
-    ID_INVALID_FORMAT:"Invalid ID format"
+    EMAIIL_TOO_LONG:"Email must be 255 chareacters or less",
+    EMAIL_INVALID:"Valid email required",
+    PASSWORD_TOO_SHORT:"Password must be at least 6 characters",
+    NAME_REQUIRED:"Name is required",
+    NAME_TOO_LONG:"Name too long"
 });
 module.exports={
     MESSAGES_OPERATION,
