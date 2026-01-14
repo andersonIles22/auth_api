4444e887eba55a350ee09abf7c6d7cd1c47b5c17
Anderson <ilesanderson01@gmail.com>
2026-01-14 18:04:47 -0500

    Preparación necesaria para hacer deploy exitosamente a auth_api

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,4 @@
 const {Pool}= require('pg');
-const { ssl } = require('pg/lib/defaults');
 require('dotenv').config();
 const pool=new Pool({
     connectionString:process.env.DATABASE_URL_AUTH_DB,
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const authPublicRoutes=require('./authPublicRoutes');
 const authPrivatRoutes=require('./authPrivateRoutes');
 
