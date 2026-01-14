e559b3740d44696e6276b48038acba1983678600
Anderson <ilesanderson01@gmail.com>
2026-01-14 16:51:45 -0500

    Preparación necesaria para hacer deploy exitosamente a auth_api

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,6 +7,6 @@ const router=express.Router();
 router.post('/register',validateRegister,authController.register);
 router.post('/login',validateLogin,authController.login);
 
-//router.use(protect);
+router.use(protect);
 router.get('/allDb',authController.getAll);
 module.exports={router};
