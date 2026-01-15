7840beafd6f38aa2dc5a8e0c2397b58f32932889
Anderson <ilesanderson01@gmail.com>
2026-01-15 09:24:54 -0500

    Preparación necesaria para hacer deploy exitosamente a auth_api

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,22 +1,13 @@
-const router = require('express').Router();
+const express = require('express');
+const authController = require('../controllers/authController');
+const { validateRegister, validateLogin } = require('../middleware/validation');
+const  {authMiddleware} = require("../middleware/auth");
 
-router.post('/register', (req,res)=>{
-  res.json({ ok: 'register reached' });
-});
+const router=express.Router();
+router.post('/register',validateRegister,authController.register);
+router.post('/login',validateLogin,authController.login);
 
-module.exports = router;
+//router.use(authMiddleware);
+router.get('/allDb',authMiddleware,authController.getAll);
 
-// console.log("la ptm")
-// const express = require('express');
-// const authController = require('../controllers/authController');
-// const { validateRegister, validateLogin } = require('../middleware/validation');
-// const  authMiddleware = require("../middleware/auth");
-
-// const router=express.Router();
-// router.post('/register',validateRegister,authController.register);
-// router.post('/login',validateLogin,authController.login);
-
-// //router.use(authMiddleware);
-// router.get('/allDb',authController.getAll);
-
-// module.exports=router;
+module.exports=router;
