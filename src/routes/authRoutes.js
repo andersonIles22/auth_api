9d8b91694df48114915e06638bb76ead89e64bfa
Anderson <ilesanderson01@gmail.com>
2026-01-15 10:10:05 -0500

    Preparación necesaria para hacer deploy exitosamente a auth_api

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,12 +4,7 @@ const { validateRegister, validateLogin } = require('../middleware/validation');
 const  {authMiddleware} = require("../middleware/auth");
 
 const router=express.Router();
-
-router.get('/register',validateRegister,authController.register);
-router.post('/login',validateLogin,authController.login);
-
-router.get('/message',validateRegister,authController.getSomethin);
-router.get('/users',authController.postSomethin);
+router.post('/register',validateRegister,authController.register);
 //router.use(authMiddleware);
 router.get('/allDb',authMiddleware,authController.getAll);
 
