ff70c6dc3e59bf589549c1480101dc2e5e4e03cf
Anderson <ilesanderson01@gmail.com>
2026-01-21 19:06:10 -0500

    Añadir generación de refresh token en endpoint /login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,7 @@ const jwt=require('jsonwebtoken');
 const {error}=require('../middleware/errorHandler');
 const {HTTP_STATUS}=require('../constants/httpStatusCode');
 const {MESSAGES_OPERATION}=require('../constants/statusMessages');
+const { parseTime } = require('../helpers/parseTime');
 
 const register=async (req,res,next)=>{
     try {
@@ -49,33 +50,56 @@ const login=async(req,res,next)=>{
 try {
     const { email, password } = req.body;
     
-    // 1. Verificar que usuario existe
+    // Verificar que usuario existe
     const result = await db.query(
       'SELECT * FROM users_admin WHERE email = $1',
       [email]
     );
     
     if (result.rows.length === 0) {
-        return error(HTTP_STATUS.AUTHORIZATION_REQUIRED,MESSAGES_OPERATION.CREDENCIAL_INVALID,next);
+      return error(HTTP_STATUS.AUTHORIZATION_REQUIRED,MESSAGES_OPERATION.CREDENCIAL_INVALID,next);
     }
     
     const user = result.rows[0];
-    
-    // 2. Verificar password
+    // Verificar password
     const isMatch = await bcrypt.compare(password, user.password);
     
     if (!isMatch) {
       return error(HTTP_STATUS.AUTHORIZATION_REQUIRED,MESSAGES_OPERATION.CREDENCIAL_INVALID,next);
     }
+
+    // Generar Refresh token
+    const refreshToken=jwt.sign(
+        {id:user.id},
+        process.env.JWT_SECRET_REFRESH,
+        {expiresIn:process.env.JWT_REFRESH_EXPIRES_IN||'7d'}
+      );
     
-    // 3. Generar JWT
+    // Refresh Token Hasheada
+    const saltTokenRefresh =await bcrypt.genSalt(10);
+    const hashedTokenRefresh=await bcrypt.hash(refreshToken,saltTokenRefresh);
+    
+    // Tiempo de expiración del Refresh Token
+    const refreshTokenExpiresAt= new Date(Date.now() + parseTime(process.env.JWT_REFRESH_EXPIRES_IN||'7d'));
+    //  Generar JWT
     const token = jwt.sign(
       { id: user.id, email: user.email },
       process.env.JWT_SECRET,
-      { expiresIn: process.env.JWT_EXPIRES_IN || '24h' }
+      { expiresIn: process.env.JWT_EXPIRES_IN || '15min' }
     );
-    
-    // 4. Responder (NO enviamos password)
+    // Almacenar el Refresh Token Hasheado en la base de datos
+    await db.query(
+      `INSERT INTO refresh_token (user_id,token,expires_at) VALUES ($1,$2,$3)`,[user.id,hashedTokenRefresh,refreshTokenExpiresAt]
+    )
+
+    // Enviar Refresh Token a una cookie segura
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'Strict',
+      expires: refreshTokenExpiresAt
+    });
+    //  Responder (NO enviamos password)
     res.json({
       success: true,
       data: {
